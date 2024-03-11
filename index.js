f7032da1f03a9b85e57bdd07827d52bc51ba6e5b
Onos Ejoor <156698969+onosejoor@users.noreply.github.com>
2024-03-11 15:50:21 +0100

    Add files via upload
    Added Animation to the drums

--- index.js
+++ index.js
@@ -1,53 +1,62 @@
-var buttonsNumber = document.querySelectorAll(".drum").length;
+var buttonNumber = document.querySelectorAll(".drum").length
 
-for (var i = 0; i< buttonsNumber; i++){
+for(var i = 0; i<buttonNumber; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        var buttonsInnerHTML = this.innerHTML;
-        sound(buttonsInnerHTML);
-
+        var buttonInnerHTML = this.innerHTML;   
+        sound(buttonInnerHTML);
+        animation(buttonInnerHTML);
     });
 }
 
-document.addEventListener("keypress", function (hi) {
-    sound(hi.key)
-})
+document.addEventListener("keydown", function (hi) {
+    sound(hi.key);
+    animation(hi.key);
+});
 
 function sound (key) {
-        switch (key) {
-            case "w":
-                var tom1 = new Audio('./tom-1.mp3');
-                tom1.play();
-                break;
-        
-                case "a":
-                    var tom2 = new Audio('./tom-2.mp3');
-                tom2.play();
-                    break;
-                    
-                case "s":
-                      var tom3 = new Audio('./tom-3.mp3');
-                tom3.play();
-                   break;
-        
-                case "d":
-                      var tom4 = new Audio('./tom-4.mp3');
-                tom4.play();
-                    break;
-        
-                case "j":
-                       var crash = new Audio('./sounds_crash.mp3');
-                       crash.play();
-                    break;
-                case "k":
-                       var kick = new Audio('./kick.wav');
-                      kick.play();
-                    break;
-        
-                case "l":
-                        var snare = new Audio('./snare.mp3');
-                     snare.play();
-                    break;
-            default:
-                break;
-        }
+switch (key) {
+    case "w":
+        var tom1 = new Audio('./tom-1.mp3');
+        tom1.play();
+        break;
+
+        case "a":
+            var tom2 = new Audio('./tom-2.mp3');
+        tom2.play();
+            break;
+            
+        case "s":
+              var tom3 = new Audio('./tom-3.mp3');
+        tom3.play();
+           break;
+
+        case "d":
+              var tom4 = new Audio('./tom-4.mp3');
+        tom4.play();
+            break;
+
+        case "j":
+               var crash = new Audio('./sounds_crash.mp3');
+               crash.play();
+            break;
+        case "k":
+               var kick = new Audio('./kick.wav');
+              kick.play();
+            break;
+
+        case "l":
+                var snare = new Audio('./snare.mp3');
+             snare.play();
+            break;
+    default:
+        break;
+}
+}
+function animation (currentKey) {
+    var activeKey = document.querySelector("." + currentKey);
+    activeKey.classList.add("pressed");
+
+    setTimeout(function () {
+        activeKey.classList.remove("pressed")},
+        100);
 }
